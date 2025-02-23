b71d87180861de30b880aa3568e4f2b83edce88b
Jeferson Souza <jeffsouza.edif@gmail.com>
2025-02-23 12:51:54 -0300

    configurando appserviceprovide

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,11 @@ declare(strict_types = 1);
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Opcodes\LogViewer\Facades\LogViewer;
 
@@ -23,6 +28,39 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        $this->setupLogViewer();
+        $this->configModels();
+        $this->configCommands();
+        $this->configUrls();
+        $this->configDate();
+    }
+
+    private function setupLogViewer(): void
+    {
         LogViewer::auth(fn ($request): true => true);
+    }
+
+    private function configModels(): void
+    {
+        Model::unguard();
+        Model::shouldBeStrict();
+
+    }
+
+    private function configCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            app()->isProduction()
+        );
+    }
+
+    private function configUrls(): void
+    {
+        URL::forceHttps();
+    }
+
+    private function configDate(): void
+    {
+        Date::use(CarbonImmutable::class);
     }
 }
